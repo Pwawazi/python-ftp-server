4fe8d27001138dbbd5cc416070c314a525fe9c3b
Pwawazi <wawaziphil@gmail.com>
2023-03-09 12:14:08 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from twisted.cred.checkers import AllowAnonymousAccess, InMemoryUsernamePasswordDatabaseDontUse
+from twisted.cred.portal import Portal
+from twisted.internet import reactor
+from twisted.protocols.ftp import FTPFactory, FTPRealm
+
+checker = InMemoryUsernamePasswordDatabaseDontUse()
+checker.addUser("philip", "231231ZI")
+checker.addUser("Dante", "12345")
+checker.addUser("Abigail", "12345")
+
+portal = Portal(FTPRealm("./public", "./myUsers"), [AllowAnonymousAccess(), checker])
+
+factory = FTPFactory(portal)
+
+reactor.listenTCP(23, factory)
+reactor.run()
